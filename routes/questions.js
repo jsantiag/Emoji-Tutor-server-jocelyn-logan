43ea24705c0428bd236fcab5a1d9cce2de033dfc
Jocelyn <35234336+jsantiag@users.noreply.github.com>
2019-01-14 16:26:37 -0600

    Update questions.js

--- routes/questions.js
+++ routes/questions.js
@@ -8,17 +8,12 @@ const router = express.Router();
 
 router.get('/next', (req, res, next) => {
   const userId = req.user._id;
-  // console.log(userId);
-  // console.log(req.user);
-  // list.push(question);
-  // res.json(question);
+
   User.findOne({ _id: userId })
     .populate('list.emoji')
     .then(result => {
-      // console.log('this is result', result);
       if (result) {
         const head = result.head;
-        // console.log(head);
         const nextEmoji = result.list[head];
         res.json(nextEmoji);
       } else {
@@ -47,27 +42,17 @@ router.post('/answer', (req, res, next) => {
   } = req.body;
   User.findOne({ _id: userId })
     .then(user => {
-      // console.log(req.body);
-      //save the value of the current head
-      // save the node that you just answered
-      //find the location of the answered node based on weight
+ 
       let answeredQ = user.list[user.head];
-      // console.log('answeredQ', answeredQ);
-
+  
       let answeredQLast = user.head;
-      // console.log(userAnswer);
+    
       if (userAnswer === true) {
-        // console.log('hit correct');
         answeredQ.weight *= 2;
       } else {
         answeredQ.weight = 1;
       }
-      // console.log('user list', user.list);
-      // console.log('weight', answeredQ.weight);
-      //change the current head to whoever answered node's next 
-      //pointer is addressed to
-      //find the insertion point
-      //insert the node by changing the next pointer
+    
       user.head = answeredQ.next;
 
       let currentQ = answeredQ;
@@ -93,67 +78,6 @@ correctAnswer => emoji.emojiName
 currentEmojiId => Emoji.findbyId => answer => foundEmoji.emojiName
 weight => user.list[someval].weight
 next => user.list[someval].next
-head => user.head 
+head => user.head */
 
-// const emojis = [
-//   {
-//     '_id': '222222222222222222222200',
-//     'emojiCode': '\u{1F436}',
-//     'emojiName': 'dog',
-//   },
-//   {
-//     '_id': '222222222222222222222201',
-//     'emojiCode': '\u{1F960}',
-//     'emojiName': 'fortune cookie',
-//   },
-//   {
-//     '_id': '222222222222222222222202',
-//     'emojiCode': '\u{1F997}',
-//     'emojiName': 'cricket',
-//   },
-//   {
-//     '_id': '222222222222222222222203',
-//     'emojiCode': '\u{1F9DF}',
-//     'emojiName': 'zombie',
-//   },
-//   {
-//     '_id': '222222222222222222222204',
-//     'emojiCode': '\u{1F4A9}',
-//     'emojiName': 'poop',
-//   }
-// ];
-//user
-// {
-//   '_id': '333333333333333333333301',
-//   'username': 'jojosan',
-//   'password': '$2a$10$af0saLgnk2QbYYPYO1PMyO66ud8mHaUAdK4o/cxUsdQ7JeVAVMA8S',
-//   'list': [
-//     {
-//       'emoji': '222222222222222222222200',
-//       'weight': 1, 
-//       'next': 1
-//     },
-//     {
-//       'emoji': '222222222222222222222201',
-//       'weight': 1, 
-//       'next': 2
-//     },
-//     {
-//       'emoji': '222222222222222222222202',
-//       'weight': 1,
-//       'next': 3
-//     },
-//     {
-//       'emoji': '222222222222222222222203',
-//       'weight': 1, 
-//       'next':4
-//     },
-//     {
-//       'emoji': '222222222222222222222204',
-//       'weight': 1, 
-//       'next': 0
-//     },
-//   ],
-//   'head':0
-// }*/
 module.exports = router;
